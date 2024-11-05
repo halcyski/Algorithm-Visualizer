aecee8d6817d2d61e1afd2285e966d8fd2f92286
Halcyon Skinner <hdskinner@gmail.com>
2024-11-05 16:03:17 -0500

    Cleaned up garbage
    Implemented gitignore.

---
+++ algorithms/pathfinding.cpp
@@ -0,0 +1,1 @@
+// TODO: Implement Dijkstra's, A*
---
+++ algorithms/sorting.h
@@ -0,0 +1,11 @@
+#include <vector>
+
+void bubbleSort(std::vector<int>& array);
+
+void merge(std::vector<int>& array, int left, int mid, int right);
+
+void mergeSort(std::vector<int>& array, int left, int right);
+
+int partition(std::vector<int>& array, int low, int high);
+
+void quicksort(std::vector<int>& array, int low, int high);
